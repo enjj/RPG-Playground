13f73cf0346ce8157398fe1a9cf4a80df5880007
gkaragoz <gkaragoz96@gmail.com>
2019-10-23 02:33:11 +0300

    Create tons of Character scripts and implement 3rd Person Controller basics.

---
+++ Network-Base/Assets/Scripts/Character/CharacterStats_SO.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "CharacterStats", menuName = "ScriptableObjects/Character/CharacterStats", order = 1)]
+public class CharacterStats_SO : ScriptableObject {
+
+    [SerializeField]
+    private float _movementSpeed = 1f;
+    [SerializeField]
+    private float _rotationSpeed = 350f;
+
+    public float MovementSpeed {
+        get { return _movementSpeed; }
+        set { _movementSpeed = value; }
+    }
+    public float RotationSpeed {
+        get { return _rotationSpeed; }
+        set { _rotationSpeed = value; }
+    }
+
+}
