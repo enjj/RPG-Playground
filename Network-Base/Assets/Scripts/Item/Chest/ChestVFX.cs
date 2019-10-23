a4b38284d5ab425aa34e794844a773d0c6204715
gkaragoz <gkaragoz96@gmail.com>
2019-10-23 23:44:38 +0300

    Create ChestVFX script to implement chest open VFX.

---
+++ Network-Base/Assets/Scripts/Item/Chest/ChestVFX.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ChestVFX : MonoBehaviour {
+
+    [Header("Initializations")]
+    [SerializeField]
+    private GameObject _openVFX = null;
+    [SerializeField]
+    private float _deadTimeInSeconds = 1.5f;
+
+    public void FireVFX() {
+        _openVFX.SetActive(true);
+
+        Destroy(_openVFX, _deadTimeInSeconds);
+        Destroy(this, _deadTimeInSeconds);
+    }
+
+}
